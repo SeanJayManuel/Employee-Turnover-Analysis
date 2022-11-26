d9094831adabfc0ad754af4adba538fc2fea502b
Sean Manuel <80304233+SeanJayManuel@users.noreply.github.com>
2022-11-26 15:50:54 -0500

    update

--- src/RandomForest.py
+++ src/RandomForest.py
@@ -10,10 +10,13 @@ import pandas as pd
 import numpy as np
 import seaborn as sb
 import matplotlib.pyplot as plot
+
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn import tree
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix, mean_absolute_error, mean_squared_error
+
+
 
 
 data = pd.read_csv('HR_Employee_Measurement.csv')
@@ -38,3 +41,13 @@ randomfr = RandomForestRegressor(n_estimators=35, # 35 trees
 
 randomfr.fit(x_train, y_train)
 y_pred = randomfr.predict(x_test)
+
+attributes = x.columns
+tree1 = randomfr.estimators_[0]
+
+plot.figure(figsize=(15,6))
+tree.plot_tree(tree1,
+               feature_names=attributes,
+               fontsize=8, 
+               filled=True, 
+               rounded=True);
