892bbad11ff32a1e2b3d66a3d2c4cb4900d657e8
Sean Manuel <80304233+SeanJayManuel@users.noreply.github.com>
2022-11-26 21:18:53 -0500

    update

--- src/RandomForest.py
+++ src/RandomForest.py
@@ -6,7 +6,9 @@ Welcome to the random forest classifier.
 ------------
 
 """
+from lib2to3.pgen2.pgen import DFAState
 import pandas as pd
+from pandas import DataFrame
 import numpy as np
 import seaborn as sb
 import matplotlib.pyplot as plot
@@ -23,12 +25,18 @@ data = pd.read_csv('HR_Employee_Measurement.csv')
 
 print(data['Left'].unique())
 
+
+print(data['Departments'].unique())
+# Random Forest Regresor
+SEED = 45
+
+data['Salary'] = data['Salary'].replace('low', 0).replace('medium', 1).replace('high', 2)
+data['Departments'] = data['Departments'].replace('sales', 0).replace('accounting', 1).replace('hr',2).replace('technical',3).replace('support',4).replace('management',5).replace('IT',6).replace('product_mng',7).replace('marketing',8).replace('RandD', 9)
+
 y = data['Left']
 x = data.drop(['Left'], axis=1)
 
-
-# Random Forest Regresor
-SEED = 45
+print(data)
 x_train, x_test, y_train, y_test = train_test_split(x, y, 
                                                     test_size=0.25, 
                                                     random_state=SEED)
@@ -39,7 +47,9 @@ randomfr = RandomForestRegressor(n_estimators=35, # 35 trees
                              random_state=SEED)
 
 
+
 randomfr.fit(x_train, y_train)
+
 y_pred = randomfr.predict(x_test)
 
 attributes = x.columns
