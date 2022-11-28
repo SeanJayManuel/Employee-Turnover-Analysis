7dea06bb0d95c0f5189fb6864cfe47af970a4956
Sean Manuel <80304233+SeanJayManuel@users.noreply.github.com>
2022-11-28 18:11:04 -0500

    functions

--- src/Analysis.py
+++ src/Analysis.py
@@ -33,7 +33,7 @@ data.info()
 ex_employees = data.groupby('Left')
 ex_employees.mean()
 
-def figure_1():
+def left_chart():
 # This chart shows us who left the company
     Left = data.groupby('Left').count()
     plot.title("Employees Who Left vs. Total # of Employees ")
@@ -44,35 +44,37 @@ def figure_1():
 
 print(data.Left.value_counts())
 
-# This chart measures the relationship between time spent at the company
-# and number of employees.
-Tenure=data.groupby('Tenure').count()
-plot.title("Years Spent in Company vs. # of Employees")
-plot.bar(Tenure.index.values, Tenure['Satisfaction'])
-plot.xlabel('Number of Years Spent in Company')
-plot.ylabel('Number of Employees')
-plot.show()
+def tenure_chart():
+    # This chart measures the relationship between time spent at the company
+    # and number of employees.
+    Tenure = data.groupby('Tenure').count()
+    plot.title("Years Spent in Company vs. # of Employees")
+    plot.bar(Tenure.index.values, Tenure['Satisfaction'])
+    plot.xlabel('Number of Years Spent in Company')
+    plot.ylabel('Number of Employees')
+    plot.show()
 
-
-# This displays the the relationship between hours worked and the number of employees.
-Monthly_Hours=data.groupby('Monthly_Hours').count()
-plot.title("Hours Worked per Month vs. # of Employees ")
-plot.bar(Monthly_Hours.index.values, Monthly_Hours['Satisfaction'])
-plot.xlabel('Average Monthly Hours Worked')
-plot.ylabel('Number of Employees')
-plot.show()
+def hours_chart():
+    # This displays the the relationship between hours worked and the number of employees.
+    Monthly_Hours = data.groupby('Monthly_Hours').count()
+    plot.title("Hours Worked per Month vs. # of Employees ")
+    plot.bar(Monthly_Hours.index.values, Monthly_Hours['Satisfaction'])
+    plot.xlabel('Average Monthly Hours Worked')
+    plot.ylabel('Number of Employees')
+    plot.show()
 
 # Cluster graph 
 left_emp =  data[['Satisfaction', 'Evaluation']][data.Left == 1]
 kmeans = KMeans(n_clusters = 3, random_state = 0).fit(left_emp)
 left_emp['label'] = kmeans.labels_
 
-# Creates a scatter plot of three groups of employees
-plot.scatter(left_emp['Satisfaction'], left_emp['Evaluation'], c=left_emp['label'],cmap='Accent')
-plot.xlabel('Satisfaction Level')
-plot.ylabel('Last Evaluation')
-plot.title('3 Clusters of Employees Who Left')
-plot.show()
+def cluster_graph():
+    # Creates a scatter plot of three groups of employees
+    plot.scatter(left_emp['Satisfaction'], left_emp['Evaluation'], c=left_emp['label'],cmap='Accent')
+    plot.xlabel('Satisfaction Level')
+    plot.ylabel('Last Evaluation')
+    plot.title('3 Clusters of Employees Who Left')
+    plot.show()
 
 
 # Using Seaborn creates a sub plot
@@ -96,7 +98,7 @@ plot.show()
 
 
 def main():
-    figure_1()
+    left_chart()
     print("File Executed")
 
 if __name__ == "__main__":
