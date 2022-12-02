94b451cd53ae7cbcd2caf1b564c19147c1e53d7f
Sean Manuel <80304233+SeanJayManuel@users.noreply.github.com>
2022-12-01 21:50:00 -0500

    finding data points

--- src/KNearestNeighbor.py
+++ src/KNearestNeighbor.py
@@ -20,3 +20,26 @@ data = pd.read_csv('HR_Employee_Measurement.csv')
 data['Salary'] = data['Salary'].replace('low', 0).replace('medium', 1).replace('high', 2)
 data['Department'] = data['Department'].replace('sales', 0).replace('accounting', 1).replace('hr',2).replace('technical',3).replace('support',4).replace('management',5).replace('IT',6).replace('product_mng',7).replace('marketing',8).replace('RandD', 9)
 
+y = data['Left']
+X = data.drop(['Left'], axis = 1)
+
+correlation_matrix = data.corr()
+print(correlation_matrix["Left"])
+
+
+new_data_point = np.array([
+    -0.388375,
+    0.006567,
+    0.023787,
+    0.071287,
+    0.144822,
+    -0.154622,
+    -0.061788,
+    -0.043814,
+    -0.157898])
+
+distances = np.linalg.norm(X - new_data_point, axis=1)
+
+k = 3
+nearest_neighbor_ids = distances.argsort()[:k]
+print(nearest_neighbor_ids)
